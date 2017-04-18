3e7e5e02141830811e6643680ca90147b9a4c31a
Dakota <bazookaharwell@gmail.com>
2017-04-18 10:07:36 -1000

    Create a working query and add images to DOM

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,16 @@ function getGettyData(callback, query, headers = {}) {
 }
 
 function test() {
-  console.log(this.responseText);
+  const responseText = JSON.parse(this.responseText);
+  for (let i = 0; i < responseText.result_count; ++i) {
+    if (i > 30) {
+      break;
+    }
+    let image = document.createElement('img');
+    image.src = responseText.images[i].display_sizes[0].uri;
+    console.log(document.querySelector('#img-container'));
+    document.querySelector('#img-container').appendChild(image);
+  }
 }
 console.log('test');
 getGettyData(test, 'https://api.gettyimages.com/v3/search/images?phrase=video%20games', {'Api-Key': API_KEY});
